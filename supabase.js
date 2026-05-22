39ba39e6010cf677106a595101973e5100b8e44c
SergioCer <125737693+SergioCer@users.noreply.github.com>
2026-05-22 11:21:26 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 // supabase.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// Connessione centrale
-const supabaseUrl = 'https://lvtywxtunfmnkmwowxnh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2dHl3eHR1bmZtbmttd293eG5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3Njg4MDcsImV4cCI6MjA5NDM0NDgwN30.qq1qPdUxYFcqnv4cA50JMyTBAA5XWKxruL6HEE4-ACM';
+// Progetto e chiave Anon di supabase
+const supabaseUrl = 'https://ridtujvdsjonbbcrezdb.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpZHR1anZkc2pvbmJiY3JlemRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MDQyMTAsImV4cCI6MjA5NDk4MDIxMH0.eVNnqEUTmeE8t26jrx0urblPXqiHurF5LFpfyKZFMwg';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
